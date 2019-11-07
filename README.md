4f35348e44663d840414a0fadf78308ea35316d2
David Lublink <github.com@spam.lublink.net>
2019-11-06 21:42:48 -0500

    * Made functional piano using arduino as a key scanner

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # arduino_piano_keyboard
 Read an 88 key ( 176 button ) matrix and send to serial interface changes.
+
+
+It works with 8x8 keys.
+
+on raspberry pi I am running : 
+   fluidsynth -a alsa /usr/share/sounds/sf2/FluidR3_GM.sf2 < /dev/ttyUSB0
+
+   
--- key-reader.ino
+++ key-reader.ino
@@ -1,19 +1,38 @@
-const int ROWS = 4;
-const int COLS = 4;
+const int ROWS = 8;
+const int COLS = 8;
 const int KEYS = 78;
-//byte cols[8] = { 5,6,7,8,9,10,11,12};
-//byte rows[8] = { 22,24,26,28,30,32,34,36 };
+byte rows[8] = { 22,24,26,28,30,32,34,36};
+byte cols[8] = { 23,25,27,29,31,33,35,37 };
 
-byte cols[COLS] = { 11, 10, 9, 8 };
-byte rows[ROWS] = { 7,6,5,4 };
+//byte cols[COLS] = { 29, 27, 25, 23 };
+//byte rows[ROWS] = { 37, 35, 33, 31 };
+int keys[KEYS + 1] ;
+long int times[KEYS + 1] ;
 
-byte keys[KEYS + 1] ;
+int timer ;
+
+
+
+
+int buttons[ROWS][COLS] = {
+ //  0  1  2  3  4  5  6  7
+  { -45,-47,-48,-50,-49,-51,-46,-44}, // 0
+  { -53,-55,-56,-58,-57,-59,-54,-52}, // 1
+  { -61,-63,-64,-66,-65,-67,-62,-60}, // 2
+  { -69,-71,-72,-74,-73,-75,-70,-68}, // 3
+  { 45, 47,48,50,49,51,46,44}, // 4
+  { 53,55,56,58,57,59,54,52}, // 5
+  { 69,71,72,74,73,75,70,68}, // 6
+  { 61,63,64,66,65,67,62,60}, // 7
+};
+
+
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin( 9600 );
-  Serial.println("What up world");
+  Serial.println("#What up world");
   for ( int k = 0; k <= KEYS ; k++ )
   {
     keys[k]= 1;
@@ -33,10 +52,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-delay(50);
-
-  
-  
+//delay(500);
+ // pause on each loop to make debugging easier
+ //  delay(200);
   int buttonState ;
   int key;
   for ( int c = 0; c < COLS ; c++ )
@@ -49,17 +67,38 @@ delay(50);
 
         if ( buttonState != keys[key] )
         {
-          Serial.print( "====>" );
-          Serial.print( c );
-          Serial.print(" x " );
-          Serial.print( r );
-          Serial.print(" = ");
-          Serial.println( buttonState );
+          if ( buttonState == LOW )
+          {
+
+            if ( buttons[r][c] > 0 )
+              times[ buttons[r][c] ] = micros();
+            else if ( times[abs(buttons[r][c]) ] > 0 )
+            {
+              Serial.print("noteon 1 ");
+              Serial.print( abs(buttons[r][c]));
+              Serial.print(" ");
+              Serial.print( 127 - min((micros() - times[abs(buttons[r][c])])/1000,127) );
+              Serial.println("");
+            }
+          
+          }
+          else
+          {
+            if ( buttons[r][c] > 0 )
+            {
+              Serial.print("noteoff 1 ");
+              Serial.println(buttons[r][c]);
+          
+          
+            }         
+          
+          }
           keys[key] = buttonState;
         }
       }
     
     digitalWrite( cols[c], HIGH  );
   }
- 
+
+  
 }
